71a7c983c01485cbfb0be6f9dab327100e55b068
Lucas Rocha <lucasregodarocha@outlook.com>
2023-02-12 10:07:45 -0300

    feat: jpa repository, depedency injection and database seeding

---
+++ src/main/java/com/myApp/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.myApp.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myApp.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
+ 
