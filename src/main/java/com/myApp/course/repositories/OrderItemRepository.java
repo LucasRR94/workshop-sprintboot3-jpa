dae135e2f58c77c69f053f059c244fe040e6cb1d
Lucas Rocha <lucasregodarocha@outlook.com>
2023-02-25 12:39:11 -0300

    feat: create OrderItem, entity, service and repository

---
+++ src/main/java/com/myApp/course/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.myApp.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myApp.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+
+}
+ 
